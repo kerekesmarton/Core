2445aa24a730839758bdac4a530dd04620104a05
Kerekes Marton <kerekes.j.marton@gmail.com>
2020-02-18 13:42:07 +0100

    cleanup

--- Core/IosCore/Reusable/ViewControllers/NavigationController.swift
+++ Core/IosCore/Reusable/ViewControllers/NavigationController.swift
@@ -68,3 +68,20 @@ extension NavigationController: UIPopoverPresentationControllerDelegate, UIViewC
         return .bottom(totalHeight: 60, bottomOffset: view.safeAreaInsets.bottom)
     }
 }
+
+public class ClearNavigationController: NavigationController {
+    public override func applyStyle() {
+        let style = styleProvider?.navigation?.clear
+        view.backgroundColor = style?.backgroundColor
+        navigationBar.tintColor = style?.barTintColour
+        navigationBar.barTintColor = style?.backgroundColor
+        navigationBar.isTranslucent = style?.translucency ?? false
+        
+        navigationBar.shadowImage = nil
+        navigationBar.setBackgroundImage(nil, for: UIBarMetrics.default)
+        
+        guard let titleStyle = style?.titleLabel, let titleColor = titleStyle.color, let titleFont = titleStyle.font  else { return }
+        navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor : titleColor,
+                                             NSAttributedString.Key.font : titleFont]
+    }
+}
